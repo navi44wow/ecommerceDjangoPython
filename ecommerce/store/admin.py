498e60a5ecb55e4016b79955a39d1388c6962470
navi44ow <ivan.belev69@gmail.com>
2024-11-11 22:27:48 +0200

    advanced django admin configuration 	modified:   ecommerce/db.sqlite3 	deleted:    ecommerce/static/media/images/shirt.png 	modified:   ecommerce/store/__pycache__/admin.cpython-312.pyc 	modified:   ecommerce/store/admin.py

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -4,6 +4,13 @@ from django.contrib import admin
 
 from . models import Category, Product
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
 
-admin.site.register(Product)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
