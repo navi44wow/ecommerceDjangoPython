c08a559197057882ccdbfa8676a094f5cd8c3b69
navi44ow <ivan.belev69@gmail.com>
2024-11-14 17:08:22 +0200

    add functionality part 1: 	modified:   ecommerce/cart/__pycache__/cart.cpython-312.pyc 	modified:   ecommerce/cart/__pycache__/views.cpython-312.pyc 	modified:   ecommerce/cart/cart.py 	modified:   ecommerce/cart/views.py

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -17,3 +17,17 @@ class Cart():
 
         self.cart = cart
         
+
+    def add(self, product, product_qty):
+
+        product_id = str( product.id)   
+
+        if product_id in self.cart:
+
+            self.cart[product_id]['qty'] = product_qty
+
+        else:
+
+            self.cart[product_id] = {'price': str(product.price), 'qty': product_qty}
+
+        self.session.modified = True
