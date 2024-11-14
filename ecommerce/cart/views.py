8c4ff35dab0b2162038830e9884bab40b5c1a318
navi44ow <ivan.belev69@gmail.com>
2024-11-14 23:21:29 +0200

    add functionality part 2: 	modified:   ecommerce/cart/__pycache__/cart.cpython-312.pyc 	modified:   ecommerce/cart/__pycache__/views.cpython-312.pyc 	modified:   ecommerce/cart/cart.py 	modified:   ecommerce/cart/views.py 	modified:   ecommerce/db.sqlite3 	modified:   ecommerce/store/templates/store/base.html 	modified:   ecommerce/store/templates/store/product-info.html

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -30,7 +30,9 @@ def cart_add(request):
 
         cart.add(product=product, product_qty=product_quantity)
 
-        response = JsonResponse({'The product is called: ': product.title, ' and the quantity is: ': product_quantity})	
+        cart_quantity = cart.__len__()
+
+        response = JsonResponse({'qty': cart_quantity})	
 
         return response
 
